3f240e8a8d70d2334e71c7b47236641fa8cc9655
Bryan J. Zhu <bjz20@ilab2.cs.rutgers.edu>
2021-03-09 20:22:58 -0500

    stil on mutex

---
+++ proj_2/code/benchmarks/test2.c
@@ -0,0 +1,48 @@
+#include <pthread.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <unistd.h> 
+#include "../rpthread.h"
+#define DEFAULT_THREAD_NUM 1
+int thread_num;
+int counter; 
+
+pthread_mutex_t lock;
+void* trythis(void* arg) 
+{
+
+	pthread_mutex_lock(&lock);
+	unsigned long i = 0;
+	counter += 1; 
+	printf("\n\n Job %d has started\n\n", counter); 
+
+	for(i = 0; i < (0x00FFFFFF);i++);//fake work (more than 5ms)
+	printf("\n\n Job %d has finished\n\n", counter);
+	pthread_mutex_unlock(&lock);
+	pthread_exit(0); 
+} 
+
+int main(int argc, char**argv) {
+	pthread_t tids[thread_num]; 
+	if (argc == 1) {
+		thread_num = DEFAULT_THREAD_NUM;
+	} else {
+		if (argv[1] < 1) {
+			printf("enter a valid thread number\n");
+			return 0;
+		} else
+			thread_num = atoi(argv[1]);
+	}	
+	int i = 0; 
+	pthread_mutex_init(&lock,NULL);
+	for(i=0;i<thread_num;i++){
+		pthread_create(&tids[i], NULL, &trythis, NULL);
+	}
+	pthread_t rando = 5;
+	for(i=0;i<thread_num;i++){
+		pthread_join(rando, NULL);
+	}
+
+	return 0;
+}
